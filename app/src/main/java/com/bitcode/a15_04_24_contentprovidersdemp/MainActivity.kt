86a8a069b92521f62c371f9559b27de9fe9ea9dc
aishwarya <ashp17star@gmail.com>
2024-07-16 13:15:50 +0530

    getAllTasks, getTaskById

--- app/src/main/java/com/bitcode/a15_04_24_contentprovidersdemp/MainActivity.kt
+++ app/src/main/java/com/bitcode/a15_04_24_contentprovidersdemp/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.bitcode.a15_04_24_contentprovidersdemp
 
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -9,14 +12,65 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var contentResolver: ContentResolver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
+        Log.e("tag", "--------------------------")
         getSystemPreferences()
+        Log.e("tag", "--------------------------")
         getSMS()
+        Log.e("tag", "--------------------------")
         getCallLog()
+
+        Log.e("tag", "--------------------------")
+        addTask(103,"Complete Assignment", 2, false)
+        addTask(104,"Complete Tests", 3, false)
+        addTask(105,"Appear For Test", 4, true)
+        addTask(106, "Meeting", 5, true)
+
+        Log.e("tag", "--------------------------")
+
+        getAllTasks()
+
+        Log.e("tag", "--------------------------")
+    }
+
+
+    private fun addTask(taskId : Int, title : String, priority : Int, isComplete : Boolean){
+        var contentUri = Uri.parse("content://in.bitcode.contents")
+        contentUri = Uri.withAppendedPath(contentUri, "tasks")
+
+        val values = ContentValues()
+        values.put("task_id", taskId)
+        values.put("task_title",title)
+        values.put("task_priority",priority)
+        values.put("task_completion", isComplete)
+
+        val uriToNewTask = contentResolver.insert(contentUri,values)
+
+        Log.e("tag", "${uriToNewTask.toString()}")
+
+        val c = contentResolver.query(uriToNewTask!!, null, null, null)
+
+        while (c!!.moveToNext()){
+            Log.e("tag","${c.getInt(0)}")
+        }
+        c.close()
+    }
+
+    private fun getAllTasks(){
+        var contentUri = Uri.parse("content://in.bitcode.contents")
+        contentUri = Uri.withAppendedPath(contentUri,"tasks")
+
+        val c = contentResolver.query(contentUri,null,null,null)
+
+        while (c!!.moveToNext()){
+            Log.e("tag", "${c.getInt(0)}  -- ${c.getString(1)}  -- ${c.getInt(2)} -- ${c.getString(3)}")
+        }
+        c.close()
     }
 
     private fun getSMS(){
